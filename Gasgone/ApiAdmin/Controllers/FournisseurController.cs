2a7654f70b42c0cf37ac0b96255484f3378c8d90
IBra-dalas <81654522+IBra-dalas@users.noreply.github.com>
2021-12-21 22:22:24 +0100

    Add files via upload

---
+++ Gasgone/ApiAdmin/Controllers/FournisseurController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+//using DataContext = Gasgone.ORM.Data;
+
+namespace Gasgone.ApiAdmin.Controllers
+{
+    [ApiController]
+    public class FournisseurController : ControllerContext
+    {
+        private readonly DataContext _bdd;
+
+    }
+}
